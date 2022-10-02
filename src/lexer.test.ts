fa0b651369a06a174465932e661caf51b140d82b
Tamas Bunth <Tamas.Bunth@swisscom.com>
2022-10-02 18:55:12 +0200

    add complex test

--- src/lexer.test.ts
+++ src/lexer.test.ts
@@ -1,4 +1,4 @@
-import { BlockBeginToken, BlockEndToken, EndOfInputToken, Lexer, StringToken } from './lexer';
+import { BlockBeginToken, BlockEndToken, EndOfInputToken, Lexer, StatementEndToken, StringToken, Token } from './lexer';
 
 import * as mocha from 'mocha';
 import * as chai from 'chai';
@@ -6,7 +6,7 @@ import * as chai from 'chai';
 const expect = chai.expect;
 describe('Yang lexer', () => {
 
-  it('should be able to add things correctly' , () => {
+  it('should parse module { }' , () => {
     let lexer = new Lexer(`module { }`)
     let token = lexer.getToken()
     expect(token).to.deep.eq(new StringToken("module"));
@@ -17,5 +17,56 @@ describe('Yang lexer', () => {
     token = lexer.getToken()
     expect(token).to.deep.eq(new EndOfInputToken());
   });
+
+  it(`should parse complex yang file`, () => {
+    let lexer = new Lexer(`module example-sports {
+
+        namespace "http://example.com/example-sports";
+        prefix sports;
+      
+        import ietf-yang-types { prefix yang; }
+      
+        typedef season {
+          type string;
+          description
+            "The name of a sports season, including the type and the year, e.g,
+             'Champions League 2014/2015'.";
+        }
+      
+        container sports {
+          config true;
+      
+          list person {
+            key name;
+            leaf name { type string; }
+            leaf birthday { type yang:date-and-time; mandatory true; }
+          }
+      
+          list team {
+            key name;
+            leaf name { type string; }
+            list player {
+              key "name season";
+              unique number;
+              leaf name { type leafref { path "/sports/person/name"; }  }
+              leaf season { type season; }
+              leaf number { type uint16; mandatory true; }
+              leaf scores { type uint16; default 0; }
+            }
+          }
+        }
+    }`)
+     
+    let current_token = lexer.getToken()
+    let count = 0
+    while(!(current_token instanceof EndOfInputToken))
+    {
+        count++
+        current_token = lexer.getToken()
+    }
+    expect(count).to.eq(120)
+  })
+
+
 
 });
