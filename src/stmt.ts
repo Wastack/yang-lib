db0c11df344f58eb537f016c3d01ed767891f30b
Tamas Bunth <Tamas.Bunth@swisscom.com>
2022-10-04 13:39:18 +0200

    add leafliststmt

--- src/stmt.ts
+++ src/stmt.ts
@@ -34,13 +34,13 @@ export class LeafStmt {
         public if_feature: string[],
         public must: MustStmt[],
         public type: UnprocessedStatement,
+        public status: StatusStmt,
 
         public when?: WhenStmt,
         public units?: string,
         public default_?: string,
         public config?: boolean,
         public mandatory?: boolean,
-        public status?: StatusStmt,
         public description?: string,
         public reference?: string
     ) { }
@@ -65,16 +65,81 @@ export class LeafStmt {
             parser.takeZeroOrMore("if-feature").map((v) => v.argumentOrError()),
             parser.takeZeroOrMore("must").map((v) => MustStmt.parse(v)),
             parser.takeOne("type"),
+            convertStatusStmt(status?.argumentOrError()),
             when == undefined ? undefined : WhenStmt.parse(when),
             units?.argumentOrError(),
             default_?.argumentOrError(),
             config == undefined ? undefined : convertBoolean(config?.argumentOrError()),
             mandatory == undefined ? undefined : convertBoolean(mandatory?.argumentOrError()),
-            convertStatusStmt(status?.argumentOrError()),
             description?.argumentOrError(),
             reference?.argumentOrError(),
         )
     }
+}
+
+export class LeafListStmt {
+    constructor(
+        public idenfifier: Identifier,
+
+        public if_feature: string[],
+        public must: MustStmt[],
+        public default_: string[],
+        public type: UnprocessedStatement,
+        public ordered_by: OrderedByStmt,
+        public status: StatusStmt,
+
+        public when?: WhenStmt,
+        public units?: string,
+        public config?: boolean,
+        public min_elements?: number,
+        public max_elements?: number,
+        public description?: string,
+        public reference?: string
+    ) {}
+
+    static parse(unprocessed: UnprocessedStatement): LeafListStmt {
+        let identifier = new Identifier(unprocessed.argumentOrError())
+
+        let parser = new UnprocessedStatementParser()
+        parser.fetch(unprocessed)
+
+        let ordered_by = parser.takeOptional("ordered-by")
+        let when = parser.takeOptional("when")
+        let units = parser.takeOptional("units")
+        let config = parser.takeOptional("config")
+        let min_elements = parser.takeOptional("min-elements")?.argumentOrError()
+        let max_elements = parser.takeOptional("max-elements")?.argumentOrError()
+        let status = parser.takeOptional("status")
+
+        let description = parser.takeOptional("description")
+        let reference = parser.takeOptional("reference")
+        return new LeafListStmt(
+            identifier,
+            parser.takeZeroOrMore("if-feature").map((v) => v.argumentOrError()),
+            parser.takeZeroOrMore("must").map((v) => MustStmt.parse(v)),
+            parser.takeZeroOrMore("default").map((v) => v.argumentOrError()),
+            parser.takeOne("type"),
+            convertOrderedByStmt(ordered_by?.argumentOrError()),
+            convertStatusStmt(status?.argumentOrError()),
+            when == undefined ? undefined : WhenStmt.parse(when),
+            units?.argumentOrError(),
+            config == undefined ? undefined : convertBoolean(config?.argumentOrError()),
+            min_elements == undefined ? undefined : convertNumber(min_elements),
+            max_elements == undefined ? undefined : convertNumber(max_elements),
+            description?.argumentOrError(),
+            reference?.argumentOrError(),
+        )
+
+    }
+}
+
+function convertNumber(text: string): number {
+    let num = +text       
+    if (Number.isNaN(num)) {
+        throw new ParserError(`expected the string token '${text}' to be a number`)
+    }
+
+    return num
 }
 
 function convertBoolean(text: string): boolean {
@@ -84,19 +149,41 @@ function convertBoolean(text: string): boolean {
         return false
     }
 
-    throw new ParserError("invalid boolean format")
+    throw new ParserError(`invalid boolean format: '${text}'`)
+}
+
+export enum OrderedByStmt {
+    system = "system", // default
+    user = "user"
+}
+
+function convertOrderedByStmt(text?: string): OrderedByStmt {
+    if (text == undefined) {
+        return OrderedByStmt.system
+    }
+
+    let ordered_by = text as keyof typeof OrderedByStmt
+    if (ordered_by != undefined && OrderedByStmt[ordered_by] == undefined) {
+        throw new ParserError(`invalid ordered-by statement '${text}', it can only be 'system' or 'user'`)
+    }
+
+    return OrderedByStmt[ordered_by]
 }
 
 export enum StatusStmt {
-    current = "current",
+    current = "current", // default
     deprecated = "deprecated",
     obsolete = "obsolete"
 }
 
-function convertStatusStmt(text?: string): StatusStmt | undefined {
+function convertStatusStmt(text?: string): StatusStmt {
+    if (text == undefined) {
+        return StatusStmt.current
+    }
+
     let status = text as keyof typeof StatusStmt
     if (status != undefined && StatusStmt[status] == undefined) {
-        throw new ParserError("invalid status, it can only be one")
+        throw new ParserError(`invalid status '${text}', it can only be one of 'current', 'deprecated' or 'obsolete'`)
     }
 
     return StatusStmt[status]
